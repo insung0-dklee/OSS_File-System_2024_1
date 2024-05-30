027fdc0602fbd88af6d4d62aa674c2e376b6f909
kimhwayeon <165128935+kimhwayeon@users.noreply.github.com>
2024-05-30 18:53:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import os
 
-file_path = 'file_to_delete.txt'
-
-# 파일 존재 여부 확인
-if os.path.exists(file_path):
-    # 파일이 존재할 경우 os.remove() 함수를 사용하여 파일 삭제
-    os.remove(file_path)
-    print(f"{file_path} 파일이 삭제되었습니다.")
-else:
-    print(f"{file_path} 파일이 존재하지 않습니다.")
-
 def getParentDir(path):
     return os.path.dirname(path)
+
+def delete_file(file_path):
+    """
+    지정된 파일 경로의 파일을 삭제하는 함수
+    
+    Args:
+        file_path (str): 삭제할 파일의 경로
+        
+    Returns:
+        str: 파일 삭제 결과 메시지
+    """
+    if os.path.exists(file_path):
+        os.remove(file_path)
+        return f"{file_path} 파일이 삭제되었습니다."
+    else:
+        return f"{file_path} 파일이 존재하지 않습니다."
 
