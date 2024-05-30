4dc33137786b7e78cf0adce4a35575dabd1f8758
SB135246 <164637682+SB135246@users.noreply.github.com>
2024-05-30 22:08:35 +0900

    따로있던 .py파일을 maim으로  통합
    위에 파일을 main파일로 통합했습니다.

--- main.py
+++ main.py
@@ -2,4 +2,20 @@ import os
 
 def getParentDir(path):
     return os.path.dirname(path)
-
+def rename_file():
+     files = os.listdir('.')
+     print("현재 디렉토리의 파일 목록:")
+     for idx, file in enumerate(files):
+         print(f"{idx+1}. {file}")
+     file_index = int(input("변경할 파일을 선택하세요 (번호 입력): ")) - 1
+     old_filename = files[file_index]
+     new_filename = input("새로운 파일 이름을 입력하세요: ")
+     try:
+         os.rename(old_filename, new_filename)
+         print(f"'{old_filename}' 파일이 '{new_filename}'로 변경되었습니다.")
+     except FileNotFoundError:
+         print("파일을 찾을 수 없습니다.")
+     except FileExistsError:
+         print("동일한 이름의 파일이 이미 존재합니다.")
+ 
+rename_file()
