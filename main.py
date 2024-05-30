1d5967e86a086bb9e086bc11c4ca9b9bc9629d1f
CokeBear99 <desked@naver.com>
2024-05-30 20:11:59 +0900

    [기능추가] 파일속성 출력 함수 추가 | txt를 pdf로 변환하는 함수 추가

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-
 import os
 import shutil
+import datetime
+from fpdf import FPDF
+
 
 def getParentDir(path):
     return os.path.dirname(path)
@@ -43,3 +45,68 @@ while not b_is_exit:
 
     else:
         print("알 수 없는 입력입니다. 다시 시도해주세요.")
+
+
+"""
+파일 속성 출력 로직
+@Param    
+    filepath : 파일 경로
+
+@Return
+    없음
+"""
+
+def ViewFileAttribute(filepath):
+    if os.path.exists(filepath):
+        file_stats = os.stat(filepath)
+        print(f"file destination : {filepath}")
+        print(f"file size : {file_stats.st_size} 바이트")
+        
+        last_mod_time = datetime.datetime.fromtimestamp(file_stats.st_mtime).strftime('%Y-%m-%d %H:%M:%S')
+        print(f"last edit time : {last_mod_time}")
+
+        creation_time = datetime.datetime.fromtimestamp(file_stats.st_ctime).strftime('%Y-%m-%d %H:%M:%S')
+        print(f"create time : {creation_time}")
+    else:
+        print(f"{filepath} 파일이 없습니다.")
+
+
+"""
+txt 파일을 PDF로 변환
+(한글 지원x | 숫자와 영어만 가능)
+cmd에서 "pip install cryptography"
+위 " " 안의 명령어를 입력 후 엔터(라이브러리 설치)
+위 과정을 거쳐 라이브러리를 설치해야 사용 가능함.
+
+@Param  
+    txt_path : txt 파일 경로
+    pdf_path : 생성될 PDF 파일 경로
+
+@Return
+    없음
+"""
+
+def Conver_Txt_to_Pdf(txt_path, pdf_path):
+    try:
+        if not os.path.exists(txt_path):
+            print(f"{txt_path} 파일이 없습니다.")
+            return
+
+        pdf = FPDF()
+        pdf.add_page()
+        pdf.set_font("Courier", size=14)
+        with open(txt_path, 'r', encoding='utf-8') as file:
+            for line in file:
+                pdf.cell(200, 20, txt=line, ln=True)
+        pdf.output(pdf_path)
+        print(f"{pdf_path}로 Pdf 변환이 성공했습니다.")
+    except Exception as e:
+        print(f"Pdf 변환 중 오류 발생 => {e}")
+
+"""
+Using Example(아래 처럼 사용시 py파일과 같은 위치에 있어야함)
+
+ViewFileAttribute("test.txt")
+Conver_Txt_to_Pdf("test.txt", "output.pdf")
+"""
+
