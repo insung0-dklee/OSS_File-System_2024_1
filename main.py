39a686921981f974f9af4d5e9e1c3956a3e67c24
CokeBear99 <desked@naver.com>
2024-05-31 02:46:34 +0900

    [기능 추가] 암호화 및 복호화 기능 추가

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+from cryptography.fernet import Fernet
 import os
 import shutil
 
@@ -11,6 +11,79 @@ def copyFile(src, dest):
         print(f"파일이 성공적으로 복사되었습니다: {dest}")
     except Exception as e:
         print(f"파일 복사 중 오류가 발생했습니다: {e}")
+
+
+"""
+=========================================
+cryptography 라이브러리 설치필요
+1.cmd 실행
+2."pip install cryptography" 입력 후 엔터
+=========================================
+Encrypt_Decrypt(file_name)
+
+| 암호화에 필요한 키 생성 | 암호화 | 복호화 기능을 가진 함수
+
+입력값 | 1 => 암호화 키생성 | 2 => 암호화 | 3 => 복호화 |
+
+@Param    
+    file_name : 암/복호화 할 경로
+    
+@Return
+    None
+ 
+"""
+
+def Encrypt_Decrypt(file_name):
+
+    sel = input("1.암호키 생성(최초1회) 2.암호화 3.복호화 : ")
+
+    if sel == "1" :
+        key = Fernet.generate_key()
+        with open("encoding.key", "wb") as key_file:
+            key_file.write(key)
+        print(f"암호화 키를 생성했습니다.")
+
+    elif sel == "2" :
+        
+        # 키 불러오기
+        with open("encoding.key", "rb") as key_file:
+            key = key_file.read()
+        fernet = Fernet(key)
+    
+        # 파일 Read
+        with open(file_name, "rb") as file:
+            data = file.read()
+        
+        # 파일 Data Encoding(암호화)
+        encryptedData = fernet.encrypt(data)
+    
+        # 암호화된 데이터를 파일에 저장(쓰기)
+        with open(file_name, "wb") as file:
+            file.write(encryptedData)
+        
+        print(f"{file_name}을 암호화 했습니다.")
+
+    elif sel == "3" :
+
+        # 키 불러오기
+        with open("encoding.key", "rb") as key_file:
+            key = key_file.read()
+        fernet = Fernet(key)
+    
+        # 암호화된 파일 읽기
+        with open(file_name, "rb") as file:
+            encryptedData = file.read()
+        
+        # 암호화된 데이터 복호화
+        decryptedData = fernet.decrypt(encryptedData)
+    
+       # 복호화된 데이터를 파일에 쓰기
+        with open(file_name, "wb") as file:
+            file.write(decryptedData)
+
+        print(f"{file_name}을 복호화 했습니다.")
+
+        
 
 b_is_exit = False
 
@@ -36,6 +109,10 @@ while not b_is_exit:
 
     elif func == "?":
         print("도움말: 1, 2, 3을 입력하여 기능을 선택하거나 '복사'를 입력하여 파일을 복사하거나 '종료'를 입력하여 종료합니다.")
+
+    elif func == "암호화 설정" :
+        file_name = input("대상 경로를 입력하세요 : " )
+        Encrypt_Decrypt(file_name)
 
     elif func.lower() == "종료":
         b_is_exit = True
