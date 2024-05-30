fbef7c2b6900f55074ff87b31c822f0681d4a128
CokeBear99 <desked@naver.com>
2024-05-30 18:25:39 +0900

    [기능추가] 파일을 이동시키는 함수 추가

--- main.py
+++ main.py
@@ -3,3 +3,27 @@ import os
 def getParentDir(path):
     return os.path.dirname(path)
 
+
+"""
+
+@Param    
+    origin : 원본 파일 경로
+    target : 이동을 원하는 파일 경로
+
+파일을 origin에서 target으로 이동시킴.
+
+# 사용 예시
+origin_path = '/path/to/source/file.txt'
+target_path = '/path/to/destination/'
+FileMove(origin_path, target_path)
+
+"""
+def FileMove(origin,target):
+
+    try:
+        shutil.move(origin, target)
+        print(f"파일 이동 {origin} => {target}")
+    except FileNotFoundError:
+        print(f"파일을 찾을 수 없음 => {origin}")
+    except Exception as e:
+        print(f"파일 이동 오류 발생 => {e}")
