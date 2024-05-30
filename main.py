68196af4d67a6bc273ecbd641c513c515ebb2dff
DongGyun Kim <kdk23538@gmail.com>
2024-05-31 00:28:47 +0900

    [기능추가]텍스트 파일을 읽거나 쓰는 기능 추가

--- main.py
+++ main.py
@@ -12,6 +12,55 @@ def copyFile(src, dest):
     except Exception as e:
         print(f"파일 복사 중 오류가 발생했습니다: {e}")
 
+
+'''
+
+    Read or Write txt file.
+    @Param
+        txt_path    :  Variables representing the path of the text
+    @return
+        None
+    
+    When use open(filename,mode)
+    Mode has 4 types.
+    r - read only
+    w - write only(overlay)
+    a - write only(add)
+    x - write only(error if present file)
+    
+'''
+
+def readOrWrite(txt_path):
+    try:
+        while True:
+            RoW = input("1: 읽기 모드, 2: 쓰기 모드(덮어쓰기), 3:쓰기 모드(덧붙이기). 4: 종료 : ")
+
+            if RoW == "1":
+                with open(txt_path, "r") as file:
+                    text = file.read()
+                    print(text)
+
+            elif RoW == "2":
+                with open(txt_path, "w") as file:
+                    text = input("적을 내용을 입력해주세요 : ")
+                    file.write(text)
+
+            elif RoW == "3":
+                with open(txt_path, "a") as file:
+                    text = input("적을 내용을 입력해주세요 : ")
+                    file.write(text)
+
+            elif RoW == "4":
+                print("텍스트 읽기 및 쓰기를 종료합니다.")
+                break
+
+            else:
+                print("잘못된 입력입니다. 다시 입력해주세요")
+
+    except Exception as e:
+        print(f"텍스트 읽기 및 쓰기 중 오류가 발생했습니다: {e}")
+
+
 b_is_exit = False
 
 while not b_is_exit:
@@ -26,8 +75,9 @@ while not b_is_exit:
         # Add functionality for option 2 here
 
     elif func == "3":
-        print("기능 3 실행.")
-        # Add functionality for option 3 here
+        print("텍스트 읽기 및 쓰기 기능 실행")
+        txt_path = input("읽거나 수정할 텍스트 경로를 입력해주세요 : ")
+        readOrWrite(txt_path)
 
     elif func == "복사":
         src = input("복사할 파일의 경로를 입력하세요: ")
@@ -35,7 +85,7 @@ while not b_is_exit:
         copyFile(src, dest)
 
     elif func == "?":
-        print("도움말: 1, 2, 3을 입력하여 기능을 선택하거나 '복사'를 입력하여 파일을 복사하거나 '종료'를 입력하여 종료합니다.")
+        print("도움말: 1, 2, 3(텍스트 읽기 및 쓰기)을 입력하여 기능을 선택하거나 '복사'를 입력하여 파일을 복사하거나 '종료'를 입력하여 종료합니다.")
 
     elif func.lower() == "종료":
         b_is_exit = True
