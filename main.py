2b567c563a05b17cf2eceae2693befc49ad93b00
PC12185\yanyo <hss85212002@gmail.com>
2024-05-31 02:01:52 +0900

    [기능추가] 디렉토리 사용량 확인

--- main.py
+++ main.py
@@ -12,6 +12,36 @@ def copyFile(src, dest):
     except Exception as e:
         print(f"파일 복사 중 오류가 발생했습니다: {e}")
 
+def get_directory_storage(dir_path = "C:\\"):
+    """
+    dir_path 매개변수를 이용하여 해당하는 디렉토리의 사용량 확인
+    dir_path에 디폴트 매개변수로 c드라이브를 설정하여
+    아무 입력이 없을 시 c드라이브의 사용량을 확인하도록 함
+
+    """
+    directory = dir_path
+    if shutil.disk_usage(directory):
+        total_space, used_space, free_space= shutil.disk_usage(directory)
+        """
+        shutil.disk_usage 함수가 리턴값으로 total, used, free 세 가지의 정보를 가진 tuple을 리턴하므로
+        리턴값을 받아주는 각각의 변수 선언
+
+        """
+        print(f"총 용량: {total_space / (1024**3):.2f}GB")
+        print(f"사용 중: {used_space / (1024**3):.2f}GB")
+        print(f"남은 공간: {free_space / (1024**3):.2f}GB")
+        """
+        위에서 받은 세 가지 값을 출력
+
+        """
+    else:
+        print("디렉토리를 찾을 수 없습니다.")    
+    """
+    디렉토리를 찾지 못할 때 출력
+    """
+
+
+
 b_is_exit = False
 
 while not b_is_exit:
