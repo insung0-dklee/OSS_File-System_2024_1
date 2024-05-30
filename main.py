aa31a22e951fcb2903f74cb6795b9ea5b94b2adf
LAPTOP-MOTF9EGN\USER <gktjdals56@naver.com>
2024-05-30 20:09:41 +0900

    [기능추가] 잘라내기(이동)기능 추가

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
+"""
+현재 경로에 특정 파일이나 디렉토리가 존재하는지를 확인하기 위해 import os를 사용
+파일을 이동이나 복사하기 위해 shutil 모듈을 사용하였음
+파일 탐색기에서의 잘라내기 기능을 구현함
+cut_file 함수는 잘라낼 파일의 경로와 붙여넣을 경로를 매개변수로 함
+이때 붙여넣을 경로에 입력이 잘못됐을 경우 에러를 발생시킴
 
+b_is_exit 변수를 0으로 초기화하고
+1을 입력하였을때 잘라내기 기능이 구현되도록 함수를 작성하였음
+
+"""
 import os
 import shutil
 
 def getParentDir(path):
     return os.path.dirname(path)
+
+def cut_file(source, destination):
+    try:
+        shutil.move(source, destination)
+        print(f"{source} 파일이 {destination}으로 잘라내기 되었습니다.")
+    except Exception as e:
+        print(f"파일을 이동하는 중 오류가 발생했습니다: {e}")
 
 def copyFile(src, dest):
     try:
@@ -18,8 +35,11 @@ while not b_is_exit:
     func = input("기능 입력 (? 입력시 도움말) : ")
 
     if func == "1":
-        print("기능 1 실행.")
-        # Add functionality for option 1 here
+        source = input("잘라낼 파일의 경로를 입력하세요: ")
+        destination = input("붙여넣을 경로를 입력하세요: ")
+        cut_file(source, destination)
+        print("잘라내기 완료")
+
 
     elif func == "2":
         print("기능 2 실행.")
@@ -35,7 +55,7 @@ while not b_is_exit:
         copyFile(src, dest)
 
     elif func == "?":
-        print("도움말: 1, 2, 3을 입력하여 기능을 선택하거나 '복사'를 입력하여 파일을 복사하거나 '종료'를 입력하여 종료합니다.")
+        print("도움말: 1을 입력하여 잘라내기(이동)하거나 2, 3을 입력하여 기능을 선택하거나 '복사'를 입력하여 파일을 복사하거나 '종료'를 입력하여 종료합니다.")
 
     elif func.lower() == "종료":
         b_is_exit = True
