54aab9d1ca19c2911e00bba818369995bf7f48a1
SeoHyun Kim <canna7610@gmail.com>
2024-05-30 22:10:10 +0900

    [기능추가] 즐겨찾기에 파일을 추가하고 그 목록을 출력하는 함수 추가

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+"""
+favorites : 즐겨찾기 목록
+addFavorite() : 원하는 파일을 즐겨찾기에 추가하는 함수
+showFavorites() : 즐겨찾기 안의 파일 목록을 순서대로 출력하는 함수
+"""
 
 import os
 import shutil
+
+favorites = []
+
 
 def getParentDir(path):
     return os.path.dirname(path)
@@ -11,6 +19,20 @@ def copyFile(src, dest):
         print(f"파일이 성공적으로 복사되었습니다: {dest}")
     except Exception as e:
         print(f"파일 복사 중 오류가 발생했습니다: {e}")
+
+def addFavorite():
+    path = input("즐겨찾기에 추가할 파일 경로를 입력하세요: ")
+    favorites.append(path)
+    print("즐겨찾기에 추가되었습니다.")
+
+def showFavorites():
+    if not favorites:
+        print("현재 즐겨찾기 목록이 비어있습니다.")
+    else:
+        print("즐겨찾기 목록:")
+        for i, favorite in enumerate(favorites, 1):
+            print(f"{i}. {favorite}")
+
 
 b_is_exit = False
 
@@ -33,6 +55,12 @@ while not b_is_exit:
         src = input("복사할 파일의 경로를 입력하세요: ")
         dest = input("복사할 위치를 입력하세요: ")
         copyFile(src, dest)
+
+    elif func == "즐겨찾기 추가":
+        addFavorite()
+
+    elif func == "즐겨찾기 목록":
+        showFavorites()
 
     elif func == "?":
         print("도움말: 1, 2, 3을 입력하여 기능을 선택하거나 '복사'를 입력하여 파일을 복사하거나 '종료'를 입력하여 종료합니다.")
