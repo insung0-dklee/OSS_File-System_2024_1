683ed370fac6b131d981cf62b7169f543b3182a9
Yoo SeungHyeon <effort-result@naver.com>
2024-05-30 18:26:08 +0900

    [기능추가] 파일을 열어 그 내용을 출력하는 함수

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ import os
 def getParentDir(path):
     return os.path.dirname(path)
 
+
+# file open feature
+def openFile(path):
+    file = open(path, 'r')
+    str = file.read()
+    return print(str)
