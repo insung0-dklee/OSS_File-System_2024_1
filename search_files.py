1150c7599fff49d82621ecabd03c939c88967984
GyeongMoKim <tag6856@gmail.com>
2024-06-01 01:24:17 +0900

    [기능추가]정규표현식을 사용하여 파일을 찾을 수 있는 함수 추가

---
+++ search_files.py
@@ -0,0 +1,16 @@
+import os
+import re
+
+def search_files(directory, name_pattern, use_regex=False):
+    found_files = []
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if use_regex:
+                if re.match(name_pattern, file):
+                    found_files.append(os.path.join(root, file))
+            else:
+                if file == name_pattern:
+                    found_files.append(os.path.join(root, file))
+    return found_files
+
+
