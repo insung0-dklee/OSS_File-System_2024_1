c10c7395df06a3757adcf75d230eff83004db0dd
yoo-seunghyeon <effort-result@naver.com>
2024-06-01 22:26:51 +0900

    [버그 수정] while loop 안에 bookmark_list가 존재해 계속해서 즐겨찾기 목록이 초기화되는 현상 해결

--- main.py
+++ main.py
@@ -365,48 +365,62 @@ def create_file(filename):
     else:
         print("비밀번호가 틀렸습니다.")
 
+def now_pwd():
+    '''
+    현위치를 지속적으로 출력하여 보여준다.
+    '''
+    return print('[] 현위치 : ' + os.getcwd())
+
+
 
 b_is_exit = False
 
+bookmark_list = []
+
+print('===========================')
+print(' [] OSS File System 2024 1')
+print('===========================')
+
 while not b_is_exit:
+    now_pwd()
+    func = input("[] 원하는 기능을 입력하세요. ('?' 입력시 도움말) \n >> ")
 
-    func = input("원하는 기능을 입력하세요. ('?' 입력시 도움말)")
-    bookmark_list = []
-
-    if func == "파일 편집":
-        print("파일 편집 기능 실행")
+    if func == "파일편집":
+        print("\n [] 파일 편집 기능 실행")
         FileEdit.file_edit()
 
     elif func == "즐겨찾기":
-        print("즐겨찾기 기능 실행.")
+        print("\n [] 즐겨찾기 기능 실행")
         Bookmark.bookmark(bookmark_list)
 
-    elif func == "파일 관리":
-        print("파일 관리 기능 실행")
+    elif func == "파일관리":
+        print("\n [] 파일 관리 기능 실행")
         FileControl.file_control()
 
     elif func == "가독성":
-        print("가독성 기능 실행")
+        print("\n [] 가독성 기능 실행")
         Readable.readable()
 
     elif func == "중복관리":
-        print("중복 관리 기능 실행")
+        print("\n [] 중복 관리 기능 실행")
         Duplicates.duplicates()
 
     elif func == "?":
         print("""
-                [도움말]
-                '파일편집' 입력시 파일을 편집할 수 있습니다.
-                '즐겨찾기' 입력시 즐겨찾기 기능을 사용할 수 있습니다.
-                '파일관리' 입력시 파일을 관리할 수 있습니다.
-                '가독성'   입력시 파일의 단위를 읽기 좋게 볼 수 있습니다.
-                '중복관리' 입력시 중복 파일을 관리할 수 있습니다.
-                '종료'     입력시 프로그램을 종료합니다.
+    ==========================================================
+    [도움말]
+    '파일편집' 입력시 파일을 편집할 수 있습니다.
+    '즐겨찾기' 입력시 즐겨찾기 기능을 사용할 수 있습니다.
+    '파일관리' 입력시 파일을 관리할 수 있습니다.
+    '가독성'   입력시 파일의 단위를 읽기 좋게 볼 수 있습니다.
+    '중복관리' 입력시 중복 파일을 관리할 수 있습니다.
+    '종료'     입력시 프로그램을 종료합니다.
+    ==========================================================
             """)
 
     elif func.lower() == "종료":
         b_is_exit = True
-        print("프로그램을 종료합니다.")
+        print("\n 프로그램을 종료합니다. \n")
 
     else:
-        print("잘못 입력하셨습니다. 다시 입력해주세요. : ")
+        print("\n 잘못 입력하셨습니다. 다시 입력해주세요. \n")
