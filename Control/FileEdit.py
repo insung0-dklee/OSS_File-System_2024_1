45305b2d1fa85e7a3f2730c6c5dc57c9ffd0f431
SeoHyun Kim <canna7610@gmail.com>
2024-06-02 01:18:50 +0900

    [기능추가] 이미 만들어진 파일의 내용 중 일부를 복사하여 다른 파일에 붙여넣는 함수를 추가

--- Control/FileEdit.py
+++ Control/FileEdit.py
@@ -5,6 +5,7 @@
 1. 파일 읽기
 2. 파일 쓰기
 3. 파일 단어 찾아 바꾸기
+4. 파일 내용 복사 및 붙여넣기
 '''
 
 def file_edit():
@@ -14,16 +15,19 @@ def file_edit():
         select = input("원하는 기능을 입력하세요. ('?' 입력시 도움말)")
 
         if select == '?':
-            print(" '읽기'        입력시 해당 파일의 내용을 출력")
-            print(" '파일생성'     입력시 파일을 생성하고 원하는 내용을 작성")
-            print(" '찾아 바꾸기'  입력시 파일을 불러오고 원하는 단어를 찾아 새 단어로 바꿀 수 있습니다")
-            print(" '종료'         입력시 프로그램을 종료할 수 있습니다.")
+            print(" '읽기'              입력시 해당 파일의 내용을 출력")
+            print(" '파일생성'          입력시 파일을 생성하고 원하는 내용을 작성")
+            print(" '찾아 바꾸기'       입력시 파일을 불러오고 원하는 단어를 찾아 새 단어로 바꿀 수 있습니다")
+            print(" '복사 및 붙여넣기'  입력시 파일을 불러오고 원하는 부분을 찾아 다른 파일에 붙여넣을 수 있습니다")
+            print(" '종료'             입력시 프로그램을 종료할 수 있습니다.")
         elif select == "읽기":
             read_file()
         elif select == "파일 생성 및 쓰기":
             create_and_write_file()
         elif select == "찾아 바꾸기":
             modify_file()
+        elif select == "복사 및 붙여넣기":
+            copy_and_paste_text()
         elif select == '종료':
             print('파일 편집 기능을 종료합니다.')
             finish = True
@@ -57,3 +61,19 @@ def modify_file():
     with open(file_path, 'w') as file:
         file.write(modified_content)
     print("찾아 바꾸기가 완료되었습니다.")
+
+def copy_and_paste_text():
+    """
+    이미 만들어진 파일의 내용 중 일부를 복사하여 다른 파일에 붙여넣는 함수
+    """
+    source_file_path = input("복사할 내용이 있는 파일의 경로를 입력하세요: ")
+    with open(source_file_path, 'r') as file:
+        content = file.read()
+    print(f"원본 파일 내용:\n{content}")
+    start_index = int(input("복사할 부분의 시작 인덱스를 입력하세요(인덱스 0부터 시작) : "))
+    end_index = int(input("복사할 부분의 끝 인덱스를 입력하세요 : "))
+    text_to_copy = content[start_index:end_index+1]
+    target_file_path = input("붙여넣을 파일의 경로를 입력하세요: ")
+    with open(target_file_path, 'a') as file:
+        file.write(text_to_copy)
+    print("복사 및 붙여넣기가 완료되었습니다.")
