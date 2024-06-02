1f880e79142b281310f275aaca82e398ec860901
jueun oh <jueun1025@naver.com>
2024-06-02 19:27:59 +0900

    [버그 수정] 가독성 > 단순변환 기능에서 발생하는 버그 수정

--- Control/Readable.py
+++ Control/Readable.py
@@ -6,7 +6,6 @@ import os
 
 
 def readable():
-
     finish = False
 
     while not finish:
@@ -20,7 +19,8 @@ def readable():
             print(" '종료'          입력시 프로그램을 종료할 수 있습니다.")
 
         elif select == '단순변환':
-            get_human_readable_size()
+            path = input("파일 경로를 입력하세요.")
+            print(get_human_readable_size(os.path.getsize(path)))
 
         elif select == '디렉토리 탐색':
             display_file_sizes()
@@ -38,18 +38,22 @@ def readable():
     매개변수 size_in_bytes: 바이트 단위의 파일 크기
     리턴값 str: 사람이 읽기 쉬운 형식으로 변환된 파일 크기
 """
-def get_human_readable_size(size_in_bytes):
 
+
+def get_human_readable_size(size_in_bytes):
     for unit in ['B', 'KB', 'MB', 'GB', 'TB']:
         if size_in_bytes < 1024:
             return f"{size_in_bytes:.2f} {unit}"
         size_in_bytes /= 1024
+
 
 """
     주어진 디렉토리의 파일 크기를 사람이 읽기 쉬운 형식으로 출력
     매개변수 directory: 디렉토리 경로
     파일 사이즈 출력
 """
+
+
 def display_file_sizes(directory):
     try:
         for filename in os.listdir(directory):
