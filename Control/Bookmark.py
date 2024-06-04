84ee31e46d875841da3866513438a021a40cddbb
SeoHyun Kim <canna7610@gmail.com>
2024-06-05 01:09:06 +0900

    [기능추가] 타입 오류를 일으키지 않도록 removeFavorite 함수 개선

--- Control/Bookmark.py
+++ Control/Bookmark.py
@@ -17,6 +17,7 @@ def bookmark(bookmark : List):
         if select == '?':
             print(" '목록' 입력시 현재 즐겨찾기 목록을 볼 수 있습니다.")
             print(" '추가' 입력시 즐겨찾기를 목록에 추가할 수 있습니다.")
+            print(" '삭제' 입력시 즐겨찾기의 항목을 삭제할 수 있습니다.")
             print(" '종료' 입력시 프로그램을 종료할 수 있습니다.")
 
         elif select == '목록':
@@ -24,6 +25,9 @@ def bookmark(bookmark : List):
 
         elif select == '추가':
             addFavorite(bookmark)
+
+        elif select == '삭제':
+            removeFavorite(bookmark)
 
         elif select == "종료":
             print("즐겨찾기를 종료합니다.")
@@ -52,22 +56,22 @@ def addFavorite(bookmark: List):
         bookmark.append(path)
         print("즐겨찾기에 추가되었습니다.")
 
-def removeFavorite():
+def removeFavorite(bookmark: List):
     """
     즐겨찾기 목록에 파일이 존재한다면 파일 번호를 포함한 즐겨찾기 목록을 출력
     제거할 파일의 번호를 입력받은 후, 해당 파일을 제거
     """
 
-    if not favorites:
+    if not bookmark:
         print("현재 즐겨찾기 목록이 비어있습니다.")
     else:
         print("즐겨찾기 목록:")
-        for i, favorite in enumerate(favorites, 1):
+        for i, favorite in enumerate(bookmark, 1):
             print(f"{i}. {favorite}")
 
         index = int(input("제거할 파일의 번호를 입력하세요: "))
-        if 1 <= index <= len(favorites):
-            removed_favorite = favorites.pop(index - 1)
+        if 1 <= index <= len(bookmark):
+            removed_favorite = bookmark.pop(index - 1)
             print(f"{removed_favorite} 가 즐겨찾기에서 제거되었습니다.")
         else:
             print("해당 번호의 파일이 존재하지 않습니다. 제거할 파일의 번호를 다시 입력해주세요.")
